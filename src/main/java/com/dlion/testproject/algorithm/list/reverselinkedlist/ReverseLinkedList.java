0b8ff324056e2d29cf5f432af6956c586d27f55a
lzy <haphlzy@163.com>
2021-10-12 17:08:32 +0800

    add

---
+++ src/main/java/com/dlion/testproject/algorithm/list/reverselinkedlist/ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package com.dlion.testproject.algorithm.list.reverselinkedlist;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+        ListNode listNode = new ListNode(0);
+        listNode.next = new ListNode(1);
+        listNode.next.next = new ListNode(2);
+        listNode.next.next.next = new ListNode(3);
+        listNode.next.next.next.next = new ListNode(4);
+        listNode.next.next.next.next.next = new ListNode(5);
+
+
+        Solution solution = new Solution();
+        ListNode listNode1 = solution.reverseBetween(listNode, 2, 4);
+        System.out.println(listNode1.next.val);
+
+    }
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+}
